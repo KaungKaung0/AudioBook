0b2e261f75a5e14c327acb61d9c9bbf064fc015f
kaungminkhant <phenomenalkaung@gmail.com>
2018-11-24 16:07:30 +0630

    Book Menu Bar Fixed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,27 +1,35 @@
 @if(isset($audio))
-		<footer class="footer fixed-bottom">
-			<div class="container">
+<footer class="footer fixed-bottom footer-controller">
+	<div class="container">
+		<div class="row">
+			<div class="col foot-book-margin">
+				<img class="footer-book-cover-size" src="{{asset("image/thumbnail/" . $thumb->thumbnail)}}" alt="" id="album_art">
+			</div>
+			<div class="col col-controller">
+				<p>{{$audio->file_name}}</p>
+			</div>
+			<div class="col col-controller">
+				<audio id="song" src="{{asset('audio/'.$audio->file_name . '.mp3')}}" ontimeupdate="initProgressBar()"></audio>
+				<span>
+					<progress id="seek-time" value="0" max="1"></progress>
+				</span>
+				<small style="float:left; position: relative; left:20px;" id="start-time"></small>
+				<small style="float:right; position: relative; right:20px;" id="end-time"></small>
+			</div>
+			<div class="col col-controller">
 				<div class="row">
-					<div class="col">
-						<img src="{{asset("image/thumbnail/" . $thumb->thumbnail)}}" alt="" id="album_art">
+					<div class="backward-fa">
+						<a href="{{route('select',['id'=>$audio->id,'ask'=>"backward"])}}" ><i class="fas fa-backward change-color-fa"></i></a>
 					</div>
-					<div class="col">
-						<p>{{$audio->file_name}}</p>
-					</div>
-					<div class="col">
-						<audio id="song" src="{{asset('audio/'.$audio->file_name . '.mp3')}}" ontimeupdate="initProgressBar()"></audio>
-						<span>
-							<progress id="seek-time" value="0" max="1"></progress>
-						</span>
-						<small style="float:left; position: relative; left:20px;" id="start-time"></small>
-						<small style="float:right; position: relative; right:20px;" id="end-time"></small>
-					</div>
-					<div class="col">
-						<a href="{{route('select',['id'=>$audio->id,'ask'=>"backward"])}}" ><i class="fas fa-backward"></i></a>
+					<div class="play-fa">
 						<i id="fa-pp" class="fas fa-play" onclick="playPause()"></i>
-						<a href="{{route('select',['id'=>$audio->id,'ask'=>"forward"])}}" id="forward"><i class="fas fa-forward"></i></a>
+					</div>
+					<div class="forward-fa">
+						<a href="{{route('select',['id'=>$audio->id,'ask'=>"forward"])}}" id="forward"><i class="fas fa-forward change-color-fa"></i></a>
 					</div>
 				</div>
-				</div>		
-		 </footer>
+			</div>
+		</div>
+	</div>		
+</footer>
 @endif
